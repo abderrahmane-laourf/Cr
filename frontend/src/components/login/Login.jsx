1c43f5d8bfceda1f47e3439cae7db99bff210114
VVVVVVVVVVVJH <152749612+VVVVVVVVVVVJH@users.noreply.github.com>
2025-12-04 10:59:39 +0100

    Fix admin redirect to employees page

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -82,7 +82,7 @@ export default function Login() {
       
       // Navigate after toast
       setTimeout(() => {
-        if (user.role === 'admin') navigate('/admin/employee')
+        if (user.role === 'admin') navigate('/admin/employees')
         else if (user.role === 'manager') navigate('/manager')
         else navigate('/employee')
       }, 1000)
